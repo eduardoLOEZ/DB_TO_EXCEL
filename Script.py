d1c604d27c68aebfda4e70dc2363f73c1217258a
Eduardo Loeza <84247208+eduardoLOEZ@users.noreply.github.com>
2023-08-19 17:10:46 -0600

    Add files via upload

--- Script.py
+++ Script.py
@@ -41,7 +41,7 @@ def main()-> None:
         'host': '127.0.0.1',
         "port": 3306,
         'user': 'root',
-        'password': 'amarillo200',
+        'password': 'password',
         'database': 'prueba_dbtoexcel'
     }
 
